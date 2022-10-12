ef38522c7c607b7bc0340cf283d623bf163cbbd4
aldiLuki <aldi.diono.ab@gmail.com>
2022-10-13 00:32:12 +0700

    membuat app say hello2

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/aldiLuki/app-say-hello2
+
+go 1.19
+
+require github.com/aldiLuki/go-say-hello2/v2 v2.0.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_hello2 "github.com/aldiLuki/go-say-hello2/v2"
+)
+
+func main() {
+	fmt.Println(go_say_hello2.SayHello2("Aldi"))
+}
